4b282776f8af6c49cdc32e27883ed9eb07a681fb
Nitish Verma <nitishcse154@gmail.com>
2023-11-27 19:00:25 +0530

    Updates

---
+++ precache-manifest.d2439ac5d860578e464d0fee5d1e5acc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00cac3de94f420a4376aa509fac58fb5",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "aee6efb5a7e7f10208ed",
+    "url": "/textutils/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "8946a0edaa5c8cb07476",
+    "url": "/textutils/static/js/2.2a068b1b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils/static/js/2.2a068b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/textutils/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "aee6efb5a7e7f10208ed",
+    "url": "/textutils/static/js/main.906ab13f.chunk.js"
+  },
+  {
+    "revision": "79ea64f27fef7de3a5b2",
+    "url": "/textutils/static/js/runtime-main.d25e19fa.js"
+  }
+]);
